01daf23cde67ce77333ce04eae30ad0fa5ed57f5
dipanshu0110 <dipanshuk55555@gmail.com>
2025-08-19 22:56:56 +0530

    add cpde

---
+++ ARRAY/movezero.java
@@ -0,0 +1,21 @@
+public class movezero {
+    public static void moveZeroes(int[] nums) {
+        int lastNonZeroFoundAt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroFoundAt++] = nums[i];
+            }
+        }
+        for (int i = lastNonZeroFoundAt; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
